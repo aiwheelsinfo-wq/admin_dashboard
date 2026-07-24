a63feb5fdf759b0efe3f04980484decb24c80c91
ansilanuuu777 <muhammedansil7777@gmail.com>
2026-07-24 13:37:35 +0530

    Check schema of driver_vendor_join_Table

--- check_vendor.php
+++ check_vendor.php
@@ -2,8 +2,8 @@
 header("Content-Type: application/json");
 include 'db_connect.php';
 
-$res = mysqli_query($conn, "SHOW TABLES");
-$tables = $res ? mysqli_fetch_all($res) : [];
+$res = mysqli_query($conn, "SHOW CREATE TABLE `driver_vendor_join_Table`");
+$schema = $res ? mysqli_fetch_assoc($res) : [];
 
-echo json_encode(["tables" => $tables]);
+echo json_encode(["schema" => $schema]);
 ?>
