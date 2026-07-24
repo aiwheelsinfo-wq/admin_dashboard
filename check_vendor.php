fc4494d53cac35c0cbf1b41ee3fb92e4a7a3fc6a
ansilanuuu777 <muhammedansil7777@gmail.com>
2026-07-24 13:36:23 +0530

    Update check_vendor.php

--- check_vendor.php
+++ check_vendor.php
@@ -2,16 +2,16 @@
 header("Content-Type: application/json");
 include 'db_connect.php';
 
-$phone = isset($_GET['phone']) ? mysqli_real_escape_string($conn, $_GET['phone']) : '9847267465';
+$vendor = $_GET['vendor'] ?? '9847267465';
 
-$vRes = mysqli_query($conn, "SELECT * FROM vendors WHERE phone_number LIKE '%$phone%'");
-$vData = $vRes ? mysqli_fetch_all($vRes, MYSQLI_ASSOC) : [];
+$jRes = mysqli_query($conn, "SELECT * FROM `driver_vendor_join_Table`");
+$jData = $jRes ? mysqli_fetch_all($jRes, MYSQLI_ASSOC) : [];
 
-$dRes = mysqli_query($conn, "SELECT * FROM drivers WHERE phone_number LIKE '%$phone%'");
+$dRes = mysqli_query($conn, "SELECT driver_id, phone_number, full_name, status, created_at FROM drivers ORDER BY driver_id DESC LIMIT 10");
 $dData = $dRes ? mysqli_fetch_all($dRes, MYSQLI_ASSOC) : [];
 
 echo json_encode([
-    "vendors" => $vData,
-    "drivers" => $dData
+    "joins" => $jData,
+    "latest_drivers" => $dData
 ]);
 ?>
