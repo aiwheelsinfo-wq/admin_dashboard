7282482f2935aaf19d40791642b079c5a5c796ff
ansilanuuu777 <muhammedansil7777@gmail.com>
2026-07-24 16:51:45 +0530

    Remove temporary fix_vendor_status.php

--- fix_vendor_status.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'db_connect.php';
-mysqli_query($conn, "UPDATE drivers SET userType='vendor', status='active' WHERE phone_number='9847267465'");
-echo json_encode(["status" => "success", "message" => "Updated 9847267465 to active vendor"]);
-?>
