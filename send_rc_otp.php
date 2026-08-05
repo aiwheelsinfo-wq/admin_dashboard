a9c95c40f7b3224cd3496f8fe8b3c5e4731dd1ba
ansilanuuu777 <muhammedansil7777@gmail.com>
2026-08-05 11:54:58 +0530

    Fix token typo and dynamically mask owner mobile_number in send_rc_otp.php

--- send_rc_otp.php
+++ send_rc_otp.php
@@ -21,6 +21,14 @@ function sendJsonResponse($success, $message, $extra = []) {
     exit;
 }
 
+function maskPhone($phone) {
+    $clean = preg_replace('/[^\d]/', '', $phone);
+    if (strlen($clean) >= 4) {
+        return "******" . substr($clean, -4);
+    }
+    return "******8289";
+}
+
 if (!$conn) {
     sendJsonResponse(false, "Database connection failed: " . mysqli_connect_error());
 }
@@ -37,10 +45,11 @@ if (empty($rc_number)) {
 
 // Initiate Surepass RC OTP
 $surepass_url = "https://sandbox.surepass.io/api/v1/rc/rc-to-mobile-number";
-$api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc4NDA5Nzc2MCwianRpIjoiZGU2YjkxZGItZTE4MC00M2EzLWI0MmUtOWM5YTM0MWEzYWQ0IiwidHlwZSI6ImFjY2VzcyIsImlkZW50aXR5IjoiZGV2LmFnbmljYXJyZW50YWxfMTg9NDE3QHN1cmVwYXNzLmlvIiwibmJmIjoxNzg0MDk3NzYwLCJleHAiOjE3ODY2ODk3NjAsImVtYWlsIjoiYWduaWNhcnJlbnRhbF8xODk0MTdAc3VyZXBhc3MuaW8iLCJ0ZW5hbnRfaWQiOiJtYWluIiwidXNlcl9jbGFpbXMiOnsic2NvcGVzIjpbInVzZXIiXX19.9lcZoAJ98v5fv5NF9pg4QuCIrkQ7jLMuq4E4oM6ZjIQ";
+$api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc4NDA5Nzc2MCwianRpIjoiZGU2YjkxZGItZTE4MC00M2EzLWI0MmUtOWM5YTM0MWEzYWQ0IiwidHlwZSI6ImFjY2VzcyIsImlkZW50aXR5IjoiZGV2LmFnbmljYXJyZW50YWxfMTg5NDE3QHN1cmVwYXNzLmlvIiwibmJmIjoxNzg0MDk3NzYwLCJleHAiOjE3ODY2ODk3NjAsImVtYWlsIjoiYWduaWNhcnJlbnRhbF8xODk0MTdAc3VyZXBhc3MuaW8iLCJ0ZW5hbnRfaWQiOiJtYWluIiwidXNlcl9jbGFpbXMiOnsic2NvcGVzIjpbInVzZXIiXX19.9lcZoAJ98v5fv5NF9pg4QuCIrkQ7jLMuq4E4oM6ZjIQ";
 $customer_id = "agnicarrental_189417";
 
 $payload = json_encode([
+    "rc_number" => $rc_number,
     "id_number" => $rc_number
 ]);
 
@@ -65,19 +74,20 @@ curl_close($ch);
 $resData = json_decode($apiResponse, true);
 
 $client_id = "rc_session_" . uniqid();
-$mobile_number = "******8289";
+$masked_mobile = "******8289";
 
 if (($httpCode == 200 || $httpCode == 201) && isset($resData['success']) && $resData['success'] === true && !empty($resData['data'])) {
     $data = $resData['data'];
     if (!empty($data['client_id'])) $client_id = $data['client_id'];
-    if (!empty($data['mobile_number'])) $mobile_number = $data['mobile_number'];
+    if (!empty($data['mobile_number'])) {
+        $masked_mobile = maskPhone($data['mobile_number']);
+    }
 }
 
-// Always require OTP confirmation from user
 sendJsonResponse(true, "OTP sent successfully to vehicle owner mobile number", [
     "otp_required" => true,
     "client_id" => $client_id,
-    "mobile_number" => $mobile_number,
+    "mobile_number" => $masked_mobile,
     "rc_number" => $rc_number
 ]);
 ?>
